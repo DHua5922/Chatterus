dee1228ce1b6baee61ee134595e582c5589839e1
Dylan Hua <hua.dylan@gmail.com>
2021-02-17 22:42:45 -0600

    CU-n2z233 - Deploy to Heroku (stage) (#1)
    CU-n2z233 - Deploy to Heroku (stage)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
-const PORT = 4000; // default port to listen
+const PORT = process.env.PORT || 4000; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-    res.send( "Node.js microservice is working now!" );
+    res.send("Node.js microservice is working now!");
 });
 
 // start the Express server
 app.listen(PORT, () => {
-    console.log( `Running at http://localhost:${PORT}` );
+    console.log(`Running at http://localhost:${PORT}`);
 });
